4a09678a0feab7d576a58e50363c1ea2cc9941f2
Roman Zherebko <romanzherebko@Mac-mini-Roman.netis>
2022-06-02 11:10:36 +0300

    init

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Roman Zherebko on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
